a9906e63549b0fe1c16b53489f46bdee43bd606f
Miika Syvänen <miika.syvanen@kolumbus.fi>
2024-09-27 13:38:50 +0300

    Add HC16_TPU memorymodel and increase timeout to serial echo check

--- serial_port/serial_port_actions_direct.cpp
+++ serial_port/serial_port_actions_direct.cpp
@@ -620,14 +620,14 @@ QByteArray SerialPortActionsDirect::write_serial_data_echo_check(QByteArray outp
             if (serial->bytesAvailable())
                 received.append(serial->read(1));
         }
-        QTime dieTime = QTime::currentTime().addMSecs(200);
+        QTime dieTime = QTime::currentTime().addMSecs(5000);
         //qDebug() << "Data sent:" << parse_message_to_hex(output);
 
         while (received.length() < output.length() && (QTime::currentTime() < dieTime))
         {
             if (serial->bytesAvailable())
             {
-                dieTime = QTime::currentTime().addMSecs(200);
+                dieTime = QTime::currentTime().addMSecs(5000);
                 received.append(serial->read(1));
             }
             QCoreApplication::processEvents(QEventLoop::AllEvents, 1);
