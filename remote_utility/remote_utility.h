b2316f32cf5cae78ec5b645ac72c46d9e2dfcaab
aalesv <aalesv70@hotmail.com>
2024-12-03 11:44:16 +0300

    Added periodic keepalive request

--- remote_utility/remote_utility.h
+++ remote_utility/remote_utility.h
@@ -21,6 +21,7 @@ public:
 public slots:
     bool send_log_window_message(QString message);
     bool set_progressbar_value(int value);
+    void ping(QString message);
     void websocket_connected(void);
     void waitForSource(void);
 
@@ -34,6 +35,10 @@ private:
     QWebSocket *webSocket;
     WebSocketIoDevice *socket;
     QRemoteObjectNode node;
+    int keepalive_interval = 10000;
+    QTimer *keepalive_timer;
+    void start_keepalive(void);
+    void stop_keepalive(void);
     void startRemote(void);
     void startOverNetwok(void);
     void startLocal(void);
